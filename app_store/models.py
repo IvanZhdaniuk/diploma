5519ee5d54d3e07bc626113aa0b59b2fd80fb0e0
IvanZhdaniuk <2819815@mail.ru>
2023-04-06 14:04:32 +0300

    1 created models

---
+++ app_store/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slag = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class CastomUser(AbstractUser):
+    phone_number = models.CharField(max_length=14, blank=True)
+    email = models.EmailField(unique=True)
+
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(CastomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quality = models.PositiveBigIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user, self.product, self.quality, self.created_at
+
+class Order(models.Model):
+    user = models.ForeignKey(CastomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quality = models.PositiveBigIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.id
