0b54c9cf32b7463b06c04fc57b7355d04fa45ae6
IvanZhdaniuk <2819815@mail.ru>
2023-04-07 09:16:55 +0300

    3 created view and error corrections

--- app_store/serializers.py
+++ app_store/serializers.py
@@ -1,8 +1,7 @@
 
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-from .models import Category, Discount, ProductItem, Producer, Promocode, \
-    RegistredUser, Order, Cashback, Comment
+from .models import Category
 
 from django.contrib.auth import authenticate
 import datetime
---
+++ app_store/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.urls import path
+from .views import CategoryListView
+
+
+urlpatterns = [
+    path('category-all/', CategoryListView.as_view()),
+
+]
