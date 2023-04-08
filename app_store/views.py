cbbea8c7a02341579348316bcc333388aad6fac3
IvanZhdaniuk <2819815@mail.ru>
2023-04-08 14:14:54 +0300

    6 added html

--- app_store/views.py
+++ app_store/views.py
@@ -10,8 +10,27 @@ class CategoryListView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (AllowAny, )
+    templates_name = 'categoris.html'
+    def get(self, request):
+        category = self.get_queryset()
+        serializer = self.get_serializer(category, many=True)
+        context = {'category': serializer.data}
+        return render(request, self.templates_name, context)
+
+
+
+
+
+
 
 class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (AllowAny, )
+    template_name = 'product_list.html'
+
+    def get(self, request):
+        products = self.get_queryset()
+        serializer = self.get_serializer(products, many=True)
+        context = {'products': serializer.data}
+        return render(request, self.template_name, context)
